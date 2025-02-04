6c4d1ffd80f6dc81b45f2eb6127793532f024f04
JanaSoliman21 <janasoliman003@gmail.com>
2025-02-04 16:32:26 +0200

    Add First Model (Test)

---
+++ Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using AIDentify.Models.Enums;
+
+namespace AIDentify.Models
+{
+    public class User
+    {
+        [Key]
+        [Column("Id")]
+        protected string UserID { get; set; }
+
+        [Required]
+        protected string UserName { get; set; }
+
+        [Required]
+        protected string FirstName { get; set; }
+
+        [Required]
+        protected string LastName { get; set; }
+
+        [Required]
+        protected string Email { get; set; }
+
+        [Required]
+        protected string Password { get; set; }
+
+        [Required]
+        protected int Age { get; set; }
+
+        [Required]
+        protected Gender Gender { get; set; }
+    }
+}
