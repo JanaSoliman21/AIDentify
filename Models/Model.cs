9bc8b1f3bb389315f789bf320eba2123daf06ca6
rehab120 <rehaabsayed@gmail.com>
2025-02-04 16:49:54 +0200

    add second model(Test)

---
+++ Models/Model.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AIDentify.Models
+{
+    public class Model
+    {
+        [Key]
+        [Column ("Id")]
+        protected string ModelID { get; set; }
+        [Required]
+        protected string ModelName { get; set; }
+
+        [Required]
+        protected object ModelItSelf { get; set; }
+
+        [Required]
+        protected string Accuracy { get; set; }
+
+        protected string GeneralFeedback { get; set; }
+
+
+        protected Review Review { get; set; }
+
+
+    }
+}
---
+++ Models/Review.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AIDentify.Models
+{
+    public class Review
+    {
+        [Key]
+        [Column ("id")]
+        protected int ReviewId { get; set; }
+
+        [Required]
+        protected string ReviewItSelf { get; set; }
+
+
+    }
+}
