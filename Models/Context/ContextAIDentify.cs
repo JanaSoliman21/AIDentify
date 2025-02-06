c39094d744a6717616f2bbca44301f7906093e55
JanaSoliman21 <janasoliman003@gmail.com>
2025-02-07 00:59:15 +0200

    Rehab Will Deal

---
+++ Models/Context/ContextAIDentify.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
+using System.Data.Common;
+
+namespace AIDentify.Models.Context
+{
+    public class ContextAIDentify: DbContext
+    {
+       public ContextAIDentify : base() { }
+    //inject
+    public BlogDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Department> Departments { get; set; }
+}
+}
